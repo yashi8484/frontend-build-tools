eba03c439c19bd58d783c90f111f549b11819bc8
yashi8484 <kobayashi@giftee.co>
2021-02-21 19:38:32 +0900

    Add rollup config for development

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -3,6 +3,39 @@ import css from 'rollup-plugin-css-only'
 import image from '@rollup/plugin-image';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import html2 from 'rollup-plugin-html2'
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import replace from '@rollup/plugin-replace';
+
+const plugins = [
+  image(),
+  css({ output: 'bundle.css' }),
+  typescript(),
+  resolve(),
+  commonjs(),
+  html2({
+    fileName: 'index.html',
+    template: 'public/index_template.html',
+    externals: [{
+      file: 'bundle.css',
+      pos: 'before'
+    }]
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }),
+];
+
+if (process.env.ROLLUP_WATCH) {
+  plugins.push(serve({
+    open: true,
+    contentBase: 'public'
+  }),
+  livereload({
+    watch: 'public'
+  }))
+}
 
 export default {
   input: 'src/index.tsx',
@@ -14,11 +47,8 @@ export default {
     inlineDynamicImports: true,
   },
   preserveEntrySignatures: false,
-  plugins: [
-    image(),
-    css({ output: 'bundle.css' }),
-    typescript(),
-    resolve(),
-    commonjs()
-  ]
+  plugins,
+  watch: {
+    clearScreen: true
+  }
 };
